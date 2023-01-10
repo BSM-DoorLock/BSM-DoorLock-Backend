b2ca41093201958aed1b4a0fd7f538b797089a2f
leehj050211 <leehj050211@gmail.com>
2023-01-11 07:56:37 +0900

    Initial commit

---
+++ src/test/java/bssm/doorlock/DoorlockApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.doorlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
